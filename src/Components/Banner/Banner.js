48745945a327fe8591963b9a9da88b73ebff8f32
Md-Roknuzzaman-khan <roknuzzamankhanrumman@gmail.com>
2021-10-18 19:44:31 +0600

    Banner Complete

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./Banner.css"
+import banner from "../../images/banner.png"
 
 const Banner = () => {
+    const bannerImg = {
+        backgroundImage: `url('${banner}')`
+    };
     return (
         <div>
-
+            <div style={bannerImg} className="banner">
+            </div>
+            <p className="bannerMoto">Welcome to our <br/> hospital</p>
         </div>
     );
 };

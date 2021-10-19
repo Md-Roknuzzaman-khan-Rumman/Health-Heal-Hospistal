7575ec5a9fcd42bdabde2e6863706bbcb12152e5
Md-Roknuzzaman-khan <roknuzzamankhanrumman@gmail.com>
2021-10-20 01:17:23 +0600

    Revert "firebase config and init in folder of firebase"
    This reverts commit be25177d0cfd94cca61d5136609cff048147c51a.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import ALlMain from "./AllMain/AllMain";
 
-const App = () => {
-	return (
-		<div className = "App">
-			<ALlMain />
-		</div>
-	);
+function App() {
+    return (
+        <div className="App">
+            <ALlMain />
+        </div>
+    );
 }
 
 export default App;
--- src/Firebase/firebase.init.js
+++
@@ -1,8 +0,0 @@
-import {initializeApp} from "firebase/firebase-app";
-import firebaseConfig from "./firebase.config";
-
-const initAuth = () => {
-	initializeApp(firebaseConfig);
-}
-
-export default initAuth;

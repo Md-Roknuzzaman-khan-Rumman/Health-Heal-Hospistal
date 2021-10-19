939a12531a690d06d9fa698ac3bb730dce8c45c8
Md-Roknuzzaman-khan <roknuzzamankhanrumman@gmail.com>
2021-10-19 10:10:03 +0600

    aboutUs section added

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./AboutUs.css"
+import banner from "../../images/aboutUsBG.jpg";
 
 const AboutUs = () => {
+	const bannerImg = {
+		backgroundImage: `url('${banner}')`
+	};
 	return (
 		<div>
-			<h1>AboutUs</h1>
+			<div style={bannerImg} className="aboutUs" />
+			<div className="aboutUsInfo">
+				<h1>What Makes Us Different</h1>
+				<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
+				   demoralized by the charms of the moment.</p>
+			</div>
 		</div>
 	);
 };

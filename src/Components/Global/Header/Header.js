5ae24869493352c93c85320b2e400c5a2493b2e8
Md-Roknuzzaman-khan <roknuzzamankhanrumman@gmail.com>
2021-10-20 20:54:28 +0600

    just commit

--- src/Components/Global/Header/Header.js
+++ src/Components/Global/Header/Header.js
@@ -3,92 +3,109 @@ import "../../../AllMain/AllMain.css"
 import "./Header.css"
 import logo from "../../../images/logo.png"
 import {NavLink} from "react-router-dom";
+import useFirebase from "../../../Hooks/useFirebase";
 
 const Header = () => {
+	const {user, logout} = useFirebase();
 	return (
 		<header>
-			<div className="navbar">
-				<div className="container nav_d-flex">
-					<div className="logo">
-						<img src={logo} alt="" />
+			<div className = "navbar">
+				<div className = "container nav_d-flex">
+					<div className = "logo">
+						<img src = {logo} alt = "" />
 					</div>
-					<nav className="navLinks">
+					<nav className = "navLinks">
 						<ul>
 							<NavLink
-								to="/home"
-								activeStyle={{
+								to = "/home"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
 								}}
-								className="nav_link">
+								className = "nav_link">
 								Home
 							</NavLink>
 							<NavLink
-								to="/aboutUs"
-								activeStyle={{
+								to = "/aboutUs"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
+								}} className = "nav_link">
 								About Us
 							</NavLink>
 							<NavLink
-								to="/services"
-								activeStyle={{
+								to = "/services"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
+								}} className = "nav_link">
 								Services
 							</NavLink>
 							<NavLink
-								to="/doctors"
-								activeStyle={{
+								to = "/doctors"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
+								}} className = "nav_link">
 								Doctors
 							</NavLink>
 							<NavLink
-								to="/contactUs"
-								activeStyle={{
+								to = "/contactUs"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
+								}} className = "nav_link">
 								Contact Us
 							</NavLink>
 							<NavLink
-								to="/ourBlogs"
-								activeStyle={{
+								to = "/ourBlogs"
+								activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
+								}} className = "nav_link">
 								Our Blogs
 							</NavLink>
 						</ul>
 					</nav>
 					
-					<div className="signedOption">
-						<div className="signInfo">
-							<NavLink
-								to="/login"
-								activeStyle={{
+					<div className = "signedOption">
+						<div className = "signInfo">
+							
+							
+							{user?.email ?
+								<div className = "whenSignedIn">
+									<NavLink to = "/login" activeStyle = {{
+										borderBottom: "2px solid #157E5",
+										fontWeight: "bold",
+										color: "white"
+									}} className = "nav_link">
+										<button onClick = {logout}>Logout</button>
+									
+									</NavLink>
+									<div className = "signImg">
+										<img src = {(user?.photoURL)} alt = "" className = "profilePic" />
+									</div>
+									<p>{user.displayName} < /p>
+								</div>
+								:
+								<NavLink to = "/login" activeStyle = {{
 									borderBottom: "2px solid #157E5",
 									fontWeight: "bold",
 									color: "white"
-								}} className="nav_link">
-								Login
-							</NavLink>
-							
-							<p>Rumman</p>
-						</div>
-						<div className="signImg">
+								}} className = "notSignedIn">LogIn
+								</NavLink>
+							}
+						
 						
 						</div>
+						{/*==============SignIn profile Pic=============*/}
+					
 					</div>
 				</div>
 			</div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,8 +3,10 @@ import "./Login.css"
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons';
+import useFirebase from "../../Hooks/useFirebase";
 
 const Login = () => {
+	const {signInUsingGoogle} = useFirebase();
 	const google = <FontAwesomeIcon icon = {faGoogle} />
 	return (
 		<div>
@@ -15,7 +17,7 @@ const Login = () => {
 				<button className = "serviceButton">Login</button>
 				<p className = "is_regi">Not Logged In! <Link to = "/register">Register</Link></p>
 				<div className = "logged_via_container">
-					<button className = "googleSignInButton">
+					<button className = "googleSignInButton" onClick = {signInUsingGoogle}>
 						<p className = "login_via">{google}<span>oogle</span></p>
 					</button>
 				</div>
